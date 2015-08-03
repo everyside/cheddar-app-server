e93dc32ce3d8c9251f588bfc175fd2bc684f4ee5
Dani Pletter <dani@fawnapp.com>
2015-08-03 12:06:31 -0700

    add namespacing

--- app.js
+++ app.js
@@ -4,19 +4,21 @@ var redis = require("redis");
 var app = express();
 var client = redis.createClient(process.env.REDIS_PORT || "6379", process.env.REDIS_HOST || "localhost");
 
-app.get('/keys/:pattern?', keys);
-app.get('/get/:key', get);
-app.get('/set/:key/:value', set)
+app.get('/:namespace/keys/:pattern?', keys);
+app.get('/:namespace/get/:key', get);
+app.get('/:namespace/set/:key/:value', set)
 
 function keys(req, res, next) {
+  var ns = req.params.namespace;
   var pattern = req.params.pattern || "*";
   console.log('getting keys matching pattern ' + pattern);
-  client.keys(pattern, function(err, keys) {
+  client.keys(ns+'.'+pattern, function(err, keys) {
     res.json(keys);
   });
 }
 
 function get(req, res, next) {
+  var ns = req.params.namespace;
   var key = req.params.key;
   console.log('getting value of key ' + key);
   client.get(key, function(err, value) {
@@ -25,10 +27,11 @@ function get(req, res, next) {
 }
 
 function set(req, res, next) {
+  var ns = req.params.namespace;
   var key = req.params.key;
   var value = req.params.value;
   console.log('setting value of key ' + key + ' to ' + value);
-  client.set(key, value, function(err, value) {
+  client.set(ns+'.'+key, value, function(err, value) {
     res.json(value);
   });
 }
