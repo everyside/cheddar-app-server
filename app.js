988e745a1518cc55b9d8ac2f775fe5283313eac7
Dani Pletter <dani@fawnapp.com>
2015-08-03 11:19:26 -0700

    git set command

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ var express = require('express');
 var redis = require("redis");
 
 var app = express();
-var client = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
+var client = redis.createClient(process.env.REDIS_PORT || "6379", process.env.REDIS_HOST || "localhost");
 
-app.get('/keys/:pattern?', getKeys);
-app.get('/key/:key', getKeyDetails);
+app.get('/keys/:pattern?', keys);
+app.get('/get/:key', get);
+app.get('/set/:key/:value', set)
 
-function getKeys(req, res, next) {
+function keys(req, res, next) {
   var pattern = req.params.pattern || "*";
   console.log('getting keys matching pattern ' + pattern);
   client.keys(pattern, function(err, keys) {
@@ -15,10 +16,19 @@ function getKeys(req, res, next) {
   });
 }
 
-function getKeyDetails(req, res, next) {
+function get(req, res, next) {
   var key = req.params.key;
   console.log('getting value of key ' + key);
   client.get(key, function(err, value) {
+    res.json(value);
+  });
+}
+
+function set(req, res, next) {
+  var key = req.params.key;
+  var value = req.params.value;
+  console.log('setting value of key ' + key + ' to ' + value);
+  client.set(key, value, function(err, value) {
     res.json(value);
   });
 }
